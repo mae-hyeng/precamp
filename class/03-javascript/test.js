926c8af93bafdfae5911333215cf698f6214f393
mae-hyeng <topjhoh20@naver.com>
2025-06-21 15:24:31 +0900

    BGM페이지 UI 구현

--- class/03-javascript/test.js
+++ class/03-javascript/test.js
@@ -15,3 +15,27 @@ function solution(genres, plays) {
 console.log(solution([5, 1, 3, 7], [2, 2, 6, 8]));
 console.log(solution([2, 2, 2, 2], [1, 1, 1, 1]));
 console.log(solution([1, 3, 9, 9], [2, 2, 8, 10]));
+
+const rn = () => {
+  const time = document.getElementById("inter");
+  const num = document.getElementById("number");
+  const btn = document.getElementById("btn");
+  let n = 3;
+  const random = Math.floor(Math.random() * 1000000)
+    .toString()
+    .padStart(6, "0");
+  num.innerHTML = random;
+  num.style.color = `#${random}`;
+
+  setInterval(() => {
+    if (n >= 0) {
+      let min = Math.floor(n / 60);
+      let sec = (n % 60).toString().padStart(2, "0");
+      time.innerHTML = `${min}:${sec}`;
+      n--;
+    } else {
+      btn.innerHTML = "인증완료";
+      btn.disabled = true;
+    }
+  }, 1000);
+};
